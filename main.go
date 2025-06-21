ffe8e8d8bd8afed8808c05b2c838d93be813f3a1
mharkness1 <mharkness.uk@gmail.com>
2025-06-21 18:34:30 +0100

    B: Chapter 1

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	linesChan := getLinesChannel(file)
+
+	for line := range linesChan {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error %v", err.Error())
+				return
+			}
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+	return lines
 }
